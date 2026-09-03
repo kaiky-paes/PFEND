b9876a416439f3b3cb927c80f4587eb959d76d08
Kaiky Paes <171882322+kaiky-paes@users.noreply.github.com>
2026-09-03 11:56:24 -0300

    Aula 03-09

---
+++ Aula_03-09/js/script.js
@@ -0,0 +1,27 @@
+    function action1() {
+        alert('evento ownkeydown')
+    }
+
+    function action2(event) {
+        alert('evento onkeypress você teclou: ' + event.key)
+    }
+
+    function action3() {
+        alert('evento onkeyup')
+    }
+
+    function action4() {
+        alert('Scroll acionado')
+    }
+
+    function action5() {
+        alert('Janela Ajustada. Tamanho: ' + window.innerHeight + 'x' + window.innerWidth)
+    }
+
+    function action6() {
+        alert('Perdeu o foco')
+    }
+
+    function action7() {
+        alert('Estado foi modificado')
+    }
